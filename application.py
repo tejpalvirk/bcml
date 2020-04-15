28060b4385f6bbc6ddb50029502eccf1787b4d01
tejpalvirk <virktj@gmail.com>
2020-04-15 10:50:29 +0000

    Commit message

--- application.py
+++ application.py
@@ -1,6 +1,31 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
+from flask_restful import reqparse, abort, Api, Resource
+import pickle
+import pandas, json
+from pyscripts import TextPreprocessor
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask(__name__)
+api = Api(app)
+
+# argument parsing
+parser = reqparse.RequestParser()
+parser.add_argument('data', required=True)
+parser.add_argument('parameters', required=True)
+
+class BCML(Resource):
+    def post(self):
+        # use parser and find the user's query
+        #args = parser.parse_args()
+        call = request.json
+        datadf = pandas.DataFrame(call['data'])
+        params = call['parameters']
+
+        if params['product'] == 'text':
+            outdf = TextPreprocessor.TextTransformer(purpose=params['purpose']).transform(datadf)
+            return outdf.to_json(orient='records')
+
+api.add_resource(BCML, '/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
