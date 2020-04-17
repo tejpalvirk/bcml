0702c66d6a0eafa6e08608955f7df4ccf918426c
tejpalvirk <virktj@gmail.com>
2020-04-17 02:47:20 +0000

    flask only

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-from flask_restful import reqparse, abort, Api, Resource
 import pickle
 import pandas, json
 from pyscripts import TextPreprocessor
@@ -9,27 +8,18 @@ os.system("python -m spacy download en_core_web_lg")
 os.system("python -m textblob.download_corpora")
 
 app = Flask(__name__)
-api = Api(app)
 
-# argument parsing
-parser = reqparse.RequestParser()
-parser.add_argument('data', required=True)
-parser.add_argument('parameters', required=True)
-
-class BCML(Resource):
-    def post(self):
-        # use parser and find the user's query
-        #args = parser.parse_args()
-        call = request.json
-        datadf = pandas.DataFrame(call['data'])
-        params = call['parameters']
-
-        if params['product'] == 'text':
-            outdf = TextPreprocessor.TextTransformer(purpose=params['purpose']).transform(datadf)
-            return outdf.to_json(orient='records')
-
-api.add_resource(BCML, '/')
+@app.route('/', methods=['POST'])
+def bcml(self):
+    if request.method == 'POST':
+        datadf = pandas.DataFrame(request.form['data'])
+        params = request.form['parameters']
+    else:
+        "InvalidRequestMethod. API only accepts POST requests"
+    if params['product'] == 'text':
+        outdf = TextPreprocessor.TextTransformer(purpose=params['purpose']).transform(datadf)
+        return outdf.to_json(orient='records')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
